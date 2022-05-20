f6ff3876f410148faeb20ce42355add7dbb0bf9c
tikokorghanashvili <101791266+tikokorghanashvili@users.noreply.github.com>
2022-05-20 20:43:10 +0400

    add

--- script.js
+++ script.js
@@ -3,28 +3,28 @@ document.getElementById('registration').addEventListener('submit',function(event
     let errors={};
     let form=event.target;
 
-    let Username=document.getElementById('username').value;
-    if (Username.length<4 || Username==" "){
-        errors.Username='username can not be empty and must be more then carachters';
+    let username=document.getElementById('username').value;
+    if (username.length<4 || username==" "){
+        errors.username='username can not be empty and must be more then 4 carachters';
     }
 
     let password=document.getElementById('passw').value;
     let password2=document.getElementById('passw2').value;
 
-    if (password.length<8 || password==" " || password!=password2){
-        errors.password='password can not be empty and must be more then carachters';
+    if (password==" " || password!=password2){
+        errors.password='password can not be empty';
         errors.password2='password do not match';
     }
 
-    let agree=document.getElementById('checkagree').Checked;
-    if (agree==false){
+    let agree=document.getElementById('checkagree').checked;
+    if (!agree){
         errors.agree='you must agree our terms and conditions';
     }
 
     // radio დროს
     let age=false;
     form.querySelectorAll('[name="age"]').forEach(element => {
-        if (element.Checked){
+        if (element.checked){
             age=true;
         }
     });
@@ -36,7 +36,7 @@ document.getElementById('registration').addEventListener('submit',function(event
     console.log(errors);
 
     form.querySelectorAll('.error-text').forEach(item=>{
-        item.innerHTML=' ';
+        item.textContent='';
     });
 
     for (let item in errors){
@@ -49,11 +49,9 @@ document.getElementById('registration').addEventListener('submit',function(event
     if(Object.keys(errors).length==0){
         form.submit();
     }
-
 });
 
-
-let passwordShow=document.getElementById('showpassword');
+let passwordShow=document.getElementById('passw2');
 let toggleIconEye=document.getElementById('toggleicon');
 
 showHidePassword=()=>{
@@ -65,8 +63,22 @@ showHidePassword=()=>{
         passwordShow.setAttribute('type','password');
     }
 }
-
 toggleIconEye.addEventListener('click',showHidePassword);
+
+
+function validation(){
+    let emailField=document.getElementById('myemail').value;
+    let ptext=document.getElementById('email');
+    let emailStructure=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (emailField.match(emailStructure)){
+        ptext.innerHTML='your email is valid';
+        ptext.style.color='green';
+    }else{
+        ptext.innerHTML='your email is invalid';
+        ptext.style.color='red';
+    }
+}
 
 
 
